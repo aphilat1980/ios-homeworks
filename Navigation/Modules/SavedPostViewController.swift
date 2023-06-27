abddb3db3f5ae96325d172a57b1ddce015be5439
Filatov Alexander <aphilat@mail.ru>
2023-06-27 21:17:55 +0500

    DZ21

---
+++ Navigation/Modules/SavedPostViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SavedPostViewController.swift
+//  Navigation
+//
+//  Created by Александр Филатов on 27.06.2023.
+//
+
+import UIKit
+
+class SavedPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Navigation/PostDataManager.swift
@@ -0,0 +1,16 @@
+//
+//  PostDataManager.swift
+//  Navigation
+//
+//  Created by Александр Филатов on 27.06.2023.
+//
+
+import Foundation
+import CoreData
+
+class PostDataManager {
+    
+    
+    
+    
+}
